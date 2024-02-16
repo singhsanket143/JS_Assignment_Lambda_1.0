0f8ec21e64fcd3f309c8c706fbdca1223c20a7c7
Manoj Kumar <manoj8894421801@gmail.com>
2024-02-16 22:15:03 +0530

    All the five questions are solved -> all the tests are also passed

--- Problems/question4.js
+++ Problems/question4.js
@@ -26,6 +26,34 @@ Read more about it here: https://lodash.com/docs/#chunk
  * @param {number} [size=1] The length of each chunk.
  * @returns {Array<Array<T>>} The new array of chunks.
  */
+
+/*
 export default function chunk(array, size = 1) {
-    throw 'Not implemented!';
-  }
+    let result = [];
+    if (size === undefined || size === null || size<=0) {
+        size = 1;
+    }
+    let i = 0;
+    while (i < array.length) {
+    //can be done using Array.slice because it return copy of portion
+        let temp = [];
+        for (let j = i; j < Math.min(size + i, array.length); j++) {
+            temp.push(array[j]);
+        }
+        result.push(temp);
+        i += size;
+    }
+    return result;
+}
+*/
+export default function chunk(array, size = 1) {
+    let result = [];
+    if (size === undefined || size === null || size <= 0) {
+        size = 1;
+    }
+
+    for (let i = 0; i < array.length; i+=size) {
+        result.push(array.slice(i, i + size));
+    }
+    return result;
+}
--- Problems/question5.js
+++ Problems/question5.js
@@ -19,6 +19,42 @@ Examples
  * @param {*} valueB
  * @return {boolean}
  */
-export default function deepEqual(valueA, valueB) {
-    throw 'Not implemented!';
+
+function compareObject(obj1, obj2) {
+  let keys1 = Object.keys(obj1);
+  let keys2 = Object.keys(obj2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
   }
+
+  return keys1.every(function cb(key, idx) {
+    return deepEqual(key, keys2[idx]) && deepEqual(obj1[key], obj2[key]);
+  });
+}
+
+function compareArray(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  return arr1.every(function cb(element, idx) {
+    return deepEqual(element, arr2[idx]);
+  });
+}
+
+export default function deepEqual(valueA, valueB) {
+
+  if (typeof valueA !== typeof valueB) {
+    return false;
+  }
+
+  if (Array.isArray(valueA)) {
+    return compareArray(valueA, valueB);
+  } else if (typeof valueA === 'object' && valueA !== null) {
+    return compareObject(valueA, valueB);
+  }
+
+  return valueA === valueB;
+
+}
