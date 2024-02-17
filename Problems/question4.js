a220516444befd869925b10710f17890210fed01
Akshat Ajay <akshat.ajay0931@gmail.com>
2024-02-17 23:47:01 +0530

    All test cases passed

--- Problems/question4.js
+++ Problems/question4.js
@@ -27,5 +27,13 @@ Read more about it here: https://lodash.com/docs/#chunk
  * @returns {Array<Array<T>>} The new array of chunks.
  */
 export default function chunk(array, size = 1) {
-    throw 'Not implemented!';
-  }
+    const chunkedArr = [];
+
+    if(array.length == 0) return chunkedArr;
+
+    for(let i=0;i<array.length;i+=size){
+        chunkedArr.push(array.slice(i, i+size))
+    }
+
+    return chunkedArr;
+}
