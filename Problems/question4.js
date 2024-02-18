b836aa22a8cd64aadf83f60ec34c6b4361bf1fe5
Roshan Kumar Sahu <roshan@reachmobile.com>
2024-02-18 19:38:38 +0530

    Assignment1 solutions

--- Problems/question4.js
+++ Problems/question4.js
@@ -1,22 +1,22 @@
 /**
- * 
-Implement a function chunk(array, [size=1]) that splits the input array into groups of length size 
-and returns them within a new array. If array can't be split evenly, the final chunk will be the 
-remaining elements. The function should not modify the original input array.
+ *
+ Implement a function chunk(array, [size=1]) that splits the input array into groups of length size
+ and returns them within a new array. If array can't be split evenly, the final chunk will be the
+ remaining elements. The function should not modify the original input array.
 
-Arguments
-    array (Array): The array to process.
-    [size=1] (number): The length of each chunk.
-Returns
-    (Array): Returns the new array of chunks.
+ Arguments
+ array (Array): The array to process.
+ [size=1] (number): The length of each chunk.
+ Returns
+ (Array): Returns the new array of chunks.
 
-Examples
-    chunk(['a', 'b', 'c', 'd']); // => [['a'], ['b'], ['c'], ['d']]
-    chunk([1, 2, 3, 4], 2); // => [[1, 2], [3, 4]]
-    chunk([1, 2, 3, 4], 3); // => [[1, 2, 3], [4]]
-    The function should return an empty array if the array argument is empty.
+ Examples
+ chunk(['a', 'b', 'c', 'd']); // => [['a'], ['b'], ['c'], ['d']]
+ chunk([1, 2, 3, 4], 2); // => [[1, 2], [3, 4]]
+ chunk([1, 2, 3, 4], 3); // => [[1, 2, 3], [4]]
+ The function should return an empty array if the array argument is empty.
 
-Read more about it here: https://lodash.com/docs/#chunk
+ Read more about it here: https://lodash.com/docs/#chunk
  */
 
 
@@ -27,5 +27,16 @@ Read more about it here: https://lodash.com/docs/#chunk
  * @returns {Array<Array<T>>} The new array of chunks.
  */
 export default function chunk(array, size = 1) {
-    throw 'Not implemented!';
-  }
+    if (!Array.isArray(array) || array.length === 0) {
+        return [];
+    }
+
+    const ans = [];
+    let i = 0;
+    while (i < array.length) {
+        ans.push(array.slice(i,i + size));
+        i  = i + size;
+    }
+
+    return ans;
+}
--- Problems/question5.js
+++ Problems/question5.js
@@ -1,17 +1,17 @@
 /**
- * 
-Implement a function deepEqual that performs a deep comparison between two 
-values. It returns true if two input values are deemed equal, and returns 
-false if not.
+ *
+ Implement a function deepEqual that performs a deep comparison between two
+ values. It returns true if two input values are deemed equal, and returns
+ false if not.
 
-You can assume there are only JSON-serializable values (numbers, strings, 
-boolean, null, objects, arrays).
-There wouldn't be cyclic objects, i.e. objects with circular references.
-Examples
-    deepEqual('foo', 'foo'); // true
-    deepEqual({ id: 1 }, { id: 1 }); // true
-    deepEqual([1, 2, 3], [1, 2, 3]); // true
-    deepEqual([{ id: '1' }], [{ id: '2' }]); // false
+ You can assume there are only JSON-serializable values (numbers, strings,
+ boolean, null, objects, arrays).
+ There wouldn't be cyclic objects, i.e. objects with circular references.
+ Examples
+ deepEqual('foo', 'foo'); // true
+ deepEqual({ id: 1 }, { id: 1 }); // true
+ deepEqual([1, 2, 3], [1, 2, 3]); // true
+ deepEqual([{ id: '1' }], [{ id: '2' }]); // false
  */
 
 /**
@@ -20,5 +20,6 @@ Examples
  * @return {boolean}
  */
 export default function deepEqual(valueA, valueB) {
+    return JSON.stringify(valueA) === JSON.stringify(valueB);
     throw 'Not implemented!';
-  }
+}
